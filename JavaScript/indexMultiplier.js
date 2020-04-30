4a59e96175e33a344f764f4494250f88f442250e
Adnan <adnan.chowdhury@outlook.com>
2020-04-29 21:15:48 -0500

    index multipler with reduce method practice

---
+++ JavaScript/indexMultiplier.js
@@ -0,0 +1,15 @@
+/*Return the sum of all items in an array, where each item is multiplied by its index (zero-based). For empty arrays, return 0.
+
+Examples
+indexMultiplier([1, 2, 3, 4, 5]) ➞ 40
+// (1*0 + 2*1 + 3*2 + 4*3 + 5*4)
+
+indexMultiplier([-3, 0, 8, -6]) ➞ -2
+// (-3*0 + 0*1 + 8*2 + -6*3)
+*/
+
+function indexMultiplier(arr) {
+  return arr.reduce((arr, num, i) => {
+    return arr + num * i;
+  }, 0);
+}
