da264f9c04eaabe526932b5bd2a7dfa3253537c7
Adnan <adnan.chowdhury@outlook.com>
2020-05-11 21:27:00 -0500

    Sample randomizer

---
+++ Python/learning/name_randomizer.py
@@ -0,0 +1,11 @@
+import random
+
+# Doc
+
+names = ['andrew', 'gustavo', 'israel', 'leana', 'andre', 'lisa', 'brennon', 'dan', 'estevan',
+         'haden', 'ira', 'jeremiah', 'michael', 'nick', 'pierre', 'preston', 'tim']
+
+random.shuffle(names)
+
+for name in names:
+    print(name)
